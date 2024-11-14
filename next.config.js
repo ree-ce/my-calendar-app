8c083e454f9a43b7d1c02f952527e18b538b4eab
Reece Chen <reece_chen@trendmicro.com>
2024-11-14 18:47:15 +0800

    setup github pages deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',  // 啟用靜態輸出
+    output: 'export',
     images: {
-      unoptimized: true,  // 對於靜態輸出必須設定
+      unoptimized: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/my-calendar-app' : '',  // 替換成你的 repository 名稱
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-calendar-app/' : '',  // 替換成你的 repository 名稱
+    basePath: process.env.NODE_ENV === 'production' ? '/my-calendar-app' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-calendar-app/' : '',
   }
 
   module.exports = nextConfig
