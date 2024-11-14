b58b63e9376358df92c471d4083cb0c3efd96115
Reece Chen <reece_chen@trendmicro.com>
2024-11-14 18:45:06 +0800

    add deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',  // 啟用靜態輸出
+    images: {
+      unoptimized: true,  // 對於靜態輸出必須設定
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/my-calendar-app' : '',  // 替換成你的 repository 名稱
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-calendar-app/' : '',  // 替換成你的 repository 名稱
+  }
 
-module.exports = nextConfig
+  module.exports = nextConfig
